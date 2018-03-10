86c576778efd118e01e87f0324fb6d81030be7fd
Kyle Fricilone <kfricilone@gmail.com>
2018-03-10 02:59:43 -0600

    Restore linux only travis

--- travis/before_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-	yes | sdkmanager "platforms;android-27"
-fi
--- travis/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-	./gradlew ios:build
-else
-	./gradlew android:build
-fi
